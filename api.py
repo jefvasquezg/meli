b27294161bd077b70dd1cc4b74595952d8cd47d2
jefvasquezg <85687617+jefvasquezg@users.noreply.github.com>
2023-03-13 10:24:35 -0500

    update file
    add now changes for the file better code

--- api.py
+++ api.py
@@ -3,35 +3,44 @@
 #flask es un framework web de python para construir aplicaciones web
 #List es un tipo de dato para almacenar una lista de elementos en python
 
-from flask import Flask, request, jsonify
-from operacion_fuego_quasar import operacion_fuego
+from flask import Flask, jsonify, request
+from .operacion_fuego_quasar import operacion_fuego
 
-app = Flask(__name__)
+class MyApp:
+    def __init__(self):
+        self.app = Flask(__name__)
 
-@app.route('/topsecret/', methods=['POST'])
-def top_secret():
-    data = request.json
-    satellites = data.get('satellites', [])
-    distances = {}
-    messages = {}
-    for satellite in satellites:
-        distances[satellite['name']] = satellite['distance']
-        messages[satellite['name']] = satellite['message']
+        # Define las rutas de la API
+        self.app.add_url_rule('/topsecret/', view_func=self.top_secret, methods=['POST'])
 
-    location = operacion_fuego.get_location(distances)
-    message = operacion_fuego.get_message(messages)
+    def run(self):
+        # Ejecuta la aplicación Flask
+        self.app.run()
 
-    if location[0] == True:
-        response = {
-            'position': {
-                'x': location[1],
-                'y': location[2]
-            },
-            'message': message
-        }
-        return jsonify(response), 200
-    else:
-        return jsonify(""), 404
+    def top_secret(self):
+        data = request.json
+        satellites = data.get('satellites', [])
+        distances = {}
+        messages = {}
+        for satellite in satellites:
+            distances[satellite['name']] = satellite['distance']
+            messages[satellite['name']] = satellite['message']
+
+        location = operacion_fuego.get_location(distances)
+        message = operacion_fuego.get_message(messages)
+
+        if location[0] == True:
+            response = {
+                'position': {
+                    'x': location[1],
+                    'y': location[2]
+                },
+                'message': message
+            }
+            return jsonify(response), 200
+        else:
+            return jsonify(""), 404
 
 if __name__ == '__main__':
+    app = MyApp()
     app.run()
